6bfe4b91bd9c36de81ac9e9a564d0eb65364ce1a
seeun7 <126846674+seeun7@users.noreply.github.com>
2026-06-23 22:27:27 +0900

    Create 씨쁠쁠.cpp

---
+++ C/씨쁠쁠.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+void main(){
+  char a = 'A'
+  int b = 7;
+  
+  cout << a << endl << b;
+} 
+// std:cout << '문자열'이 원래 방식이나 using namespace 사용으로 생략 가능. endl는 개행
+// answer: A\n7
